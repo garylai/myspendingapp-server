749310d91b031b020e1bf54df9066c67b8acf839
Gary Lai <garylaiph@gmail.com>
2015-09-04 15:54:41 +0800

    authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  before_action :authenticate
   def index
     render :nothing => true
   end
+  protected
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        true
+      end
+    end
 end
