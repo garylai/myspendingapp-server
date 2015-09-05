ef02346d820baee8b1ebd4d9094d1d98fa3e40cf
Gary Lai <garylaiph@gmail.com>
2015-09-05 17:21:59 +0800

    don’t use api only

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Myspendingapp
   class Application < Rails::Application
-    config.api_only = true
     config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
