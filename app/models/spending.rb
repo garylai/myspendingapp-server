2a1774d5c863979788351606f9d32f8a88c5285c
Gary Lai <garylaiph@gmail.com>
2015-09-07 14:10:55 +0800

    updated spending validation

--- app/models/spending.rb
+++ app/models/spending.rb
@@ -2,7 +2,7 @@ require 'UUIDHelper'
 
 class Spending < ActiveRecord::Base
   acts_as_paranoid
-  
+
   enum SpendingType: {transportation: 1, food: 2}
 
   include UUIDHelper
@@ -11,9 +11,16 @@ class Spending < ActiveRecord::Base
 
   self.primary_key = :id
   belongs_to :user
-  validates_presence_of :value, :date_of_spending, :year_of_spending, :month_of_spending, :day_of_spending, :user
-  validate :validate_spending_type
+  validate :value, :presnce => true, :numericality => true
+  validate :year_of_spending, :month_of_spending, :day_of_spending, :presence => true, :numericality: { only_integer: true }
 
+  validates_presence_of :date_of_spending, :user
+  validate :validate_spending_type
+  validate :validate_date_of_spending
+
+  def validate_date_of_spending
+    errors.add(:date_of_spending, "incorrect format") unless date_of_spending.is_a? Date
+  end
   def validate_spending_type
     errors.add(:spending_type, "not in " + Spending.SpendingTypes.keys.to_s) unless Spending.SpendingTypes.has_key?(spending_type)
   end
