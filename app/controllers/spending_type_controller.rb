027a6e809f03b064a471f672cac45ca0ad8b47fa
Gary Lai <garylaiph@gmail.com>
2015-09-17 16:10:37 +0800

    better format for to get spending_type

--- app/controllers/spending_type_controller.rb
+++ app/controllers/spending_type_controller.rb
@@ -1,6 +1,6 @@
 class SpendingTypeController < ApplicationController
   def index
     expires_in 30.minutes, :public => true
-    render :json => {:spending_type => SpendingType.all}, :status => :ok
+    render :json => SpendingType.all.select(:id, :name), :status => :ok
   end
 end
