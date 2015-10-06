ebb6f547b2ed703aabb4711b6e6af6ad625d18d6
Gary Lai <garylaiph@gmail.com>
2015-10-06 20:05:56 +0800

    add nullable first then change to not nullable

--- db/migrate/20151006111200_break_name.rb
+++ db/migrate/20151006111200_break_name.rb
@@ -1,13 +1,15 @@
 class BreakName < ActiveRecord::Migration
   def up
-    add_column :users, :first_name, :string, :null => false
-    add_column :users, :last_name, :string, :null => false
+    add_column :users, :first_name, :string, :null => true
+    add_column :users, :last_name, :string, :null => true
     User.all.each do |u|
       names = u.name.split
       u.first_name = names[0]
       u.last_name = names[1] || names[0]
       u.save
     end
+    change_column :users, :first_name, :string, :null => false
+    change_column :users, :last_name, :string, :null => false
   end
   def down
     remove_column :users, :first_name, :string
